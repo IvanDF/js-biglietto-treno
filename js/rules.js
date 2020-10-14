d69a86a31f88b1cc8907eafcc1edc90ecf3e20a7
Ivan <delfatti.ivan@gmail.com>
2020-10-14 13:31:00 +0200

    Structure

---
+++ js/rules.js
@@ -0,0 +1,15 @@
+/**
+*
+*  CALCOLATORE COSTO BIGLIETTO DEI TRENI
+*
+**/
+
+// DISTANZA DA PERCORRERE
+var kmTratta = parseInt( prompt('Distanda da percorrere') );
+console.log(kmTratta);
+
+// ETÁ DEL PASSEGGERO
+
+// CALCOLO COSTO DEL BIGLIETTO (0.21€/km)
+
+// CALCOLO SCONTO IN BASE ALL'ETÁ
