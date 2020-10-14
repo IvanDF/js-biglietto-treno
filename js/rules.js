31797cd83a271ad6367916f722415bc7ebfe128c
Ivan <delfatti.ivan@gmail.com>
2020-10-14 13:43:40 +0200

    Prezzo biglietto

--- js/rules.js
+++ js/rules.js
@@ -6,10 +6,17 @@
 
 // DISTANZA DA PERCORRERE
 var kmTratta = parseInt( prompt('Distanda da percorrere') );
-console.log(kmTratta);
+
+if (kmTratta >= 1) {
+} else {
+  alert('non é un numero é pregato di ricaricare la pagina ed inserire i dati corretti');
+}
 
 // ETÁ DEL PASSEGGERO
+var anniUtente = parseInt( prompt('Quanti anni hai') );
 
 // CALCOLO COSTO DEL BIGLIETTO (0.21€/km)
+var costoTratta = kmTratta * 0.21 + '€';
+console.log(costoTratta);
 
 // CALCOLO SCONTO IN BASE ALL'ETÁ
