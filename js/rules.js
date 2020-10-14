32a06c94628fded572dc578971bbc31192a98c30
Ivan <delfatti.ivan@gmail.com>
2020-10-14 14:01:07 +0200

    Costo totale con sconti

--- js/rules.js
+++ js/rules.js
@@ -15,8 +15,17 @@ if (kmTratta >= 1) {
 // ETÁ DEL PASSEGGERO
 var anniUtente = parseInt( prompt('Quanti anni hai') );
 
+
 // CALCOLO COSTO DEL BIGLIETTO (0.21€/km)
-var costoTratta = kmTratta * 0.21 + '€';
-console.log(costoTratta);
+var costoTratta = parseInt(kmTratta * 0.21 + '€');
 
 // CALCOLO SCONTO IN BASE ALL'ETÁ
+if (anniUtente < 18) {
+  var scontoVenti = costoTratta * 0.20;
+  costoTratta = costoTratta - scontoVenti;
+} else if (anniUtente > 65) {
+  var scontoQuaranta = costoTratta * 0.40;
+  costoTratta = costoTratta - scontoQuaranta;
+}
+
+console.log(costoTratta);
