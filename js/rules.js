e1f8667dd2d793f7f21926a769aeb67be7f459e4
Ivan <delfatti.ivan@gmail.com>
2020-10-14 16:18:40 +0200

    Grapich

--- js/rules.js
+++ js/rules.js
@@ -8,8 +8,10 @@
 var kmTratta = parseInt( prompt('Distanda da percorrere') );
 
 if (isNaN(kmTratta) === true )  {
-  alert('non é un numero é pregato di ricaricare la pagina ed inserire i dati corretti');
+  alert('I dati inseriti non sono corretti, aggiornare la pagine ed inserire correttamente i dati');
 }
+
+document.getElementById('km-tratta').innerHTML = kmTratta + 'Km';
 
 // CALCOLO COSTO DEL BIGLIETTO (0.21€/km)
 var costoTratta = parseInt(kmTratta * 0.21);
@@ -18,21 +20,26 @@ var costoTratta = parseInt(kmTratta * 0.21);
 var anniUtente = parseInt( prompt('Quanti anni hai') );
 
 if (isNaN(kmTratta) === true )  {
-  alert('non é un numero é pregato di ricaricare la pagina ed inserire i dati corretti');
+  alert('I dati inseriti non sono corretti, aggiornare la pagine ed inserire correttamente i dati');
 }
+
+document.getElementById('anni').innerHTML = anniUtente
 
 // CALCOLO SCONTO IN BASE ALL'ETÁ
 if (anniUtente < 18) {
   var scontoVenti = costoTratta * 0.20;
-  costoTratta = costoTratta - scontoVenti;
+      costoTratta = costoTratta - scontoVenti;
   alert('Hai diritto allo scondo del 20%');
+  document.getElementById('small').innerHTML = 'Hai diritto allo scondo del 20%'
 } else if (anniUtente > 65) {
   var scontoQuaranta = costoTratta * 0.40;
-  costoTratta = costoTratta - scontoQuaranta;
+      costoTratta = costoTratta - scontoQuaranta;
   alert('Hai diritto allo scondo del 40%');
+  document.getElementById('small').innerHTML = 'Hai diritto allo scondo del 40%'
 }
 
 // RISULTATO FINALE
-var prezzoFinale = costoTratta + '€';
+var prezzoFinale = costoTratta.toFixed(2) + '€';
 
 console.log(prezzoFinale);
+document.getElementById('costo-tot').innerHTML = prezzoFinale;
