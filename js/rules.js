700b3adc95237053393d569b27f7d3b667f4f824
Ivan <delfatti.ivan@gmail.com>
2020-10-14 14:29:12 +0200

    Fixes

--- js/rules.js
+++ js/rules.js
@@ -7,25 +7,32 @@
 // DISTANZA DA PERCORRERE
 var kmTratta = parseInt( prompt('Distanda da percorrere') );
 
-if (kmTratta >= 1) {
-} else {
+if (isNaN(kmTratta) === true )  {
   alert('non é un numero é pregato di ricaricare la pagina ed inserire i dati corretti');
 }
+
+// CALCOLO COSTO DEL BIGLIETTO (0.21€/km)
+var costoTratta = parseInt(kmTratta * 0.21);
 
 // ETÁ DEL PASSEGGERO
 var anniUtente = parseInt( prompt('Quanti anni hai') );
 
-
-// CALCOLO COSTO DEL BIGLIETTO (0.21€/km)
-var costoTratta = parseInt(kmTratta * 0.21 + '€');
+if (isNaN(kmTratta) === true )  {
+  alert('non é un numero é pregato di ricaricare la pagina ed inserire i dati corretti');
+}
 
 // CALCOLO SCONTO IN BASE ALL'ETÁ
 if (anniUtente < 18) {
   var scontoVenti = costoTratta * 0.20;
   costoTratta = costoTratta - scontoVenti;
+  alert('Hai diritto allo scondo del 20%');
 } else if (anniUtente > 65) {
   var scontoQuaranta = costoTratta * 0.40;
   costoTratta = costoTratta - scontoQuaranta;
+  alert('Hai diritto allo scondo del 40%');
 }
 
-console.log(costoTratta);
+// RISULTATO FINALE
+var prezzoFinale = costoTratta + '€';
+
+console.log(prezzoFinale);
